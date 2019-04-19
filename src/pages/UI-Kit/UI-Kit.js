1e02bf454f916f5dad8cbf9f10fcf01862daf08b
Leonid <leobar98@mail.ru>
2019-04-19 16:03:34 +0300

    create arrow-button

--- src/pages/UI-Kit/UI-Kit.js
+++ src/pages/UI-Kit/UI-Kit.js
@@ -1,13 +1,27 @@
 import "../../fonts/Lato-Black.scss";
+
+
 import "../../components/button/button.scss";
+
 import "../../components/button/_size/button_size_small.scss";
 import "../../components/button/_size/button_size_standard.scss";
+
 import "../../components/button/_color/button_color_cyan.scss";
 import "../../components/button/_color/button_color_red.scss";
+
 import "../../components/button/__back/button__back.scss";
 import "../../components/button/__back/_size/button__back_size_small.scss";
 import "../../components/button/__back/_size/button__back_size_standard.scss";
 import "../../components/button/__back/_color/button__back_color_cyan.scss";
 import "../../components/button/__back/_color/button__back_color_red.scss";
+
+
 import "../../components/ripple-effect/ripple-effect";
 import "../../components/ripple-effect/ripple-effect.scss";
+
+
+import "../../components/arrow-button/arrow-buttons.scss";
+import "../../components/arrow-button/arrow-buttons__arrow-container.scss";
+import "../../components/arrow-button/arrow-buttons__arrows.scss";
+import "../../components/arrow-button/_slide-animation/arrow-buttons_slide-animation.scss";
+import "../../components/arrow-button/arrow-buttons_disabled.scss";
