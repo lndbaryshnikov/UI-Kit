30894740b77a605fe1d835dda04e1ab1b523cd9d
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2019-12-29 00:19:50 +0300

    make enroll page

---
+++ src/pages/enroll/enroll-form/enroll-form.styles.js
@@ -0,0 +1,7 @@
+import "./enroll-form.scss"
+import "./enroll-form__tick-boxes.scss"
+import "./enroll-form__tick-item.scss"
+import "./enroll-form__tick-sign.scss"
+
+import "../../../components/message-form/message-form.styles"
+import "../../../components/tick-box/tick-box.styles"
