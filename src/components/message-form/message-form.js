38bdeecc94210c4936f1be692270188dec8bd83b
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-01-16 19:26:08 +0300

    make pug realization of text-field block, add modificators for message-form and search-bar and integrate tetx-field to these blocks

--- src/components/message-form/message-form.js
+++ src/components/message-form/message-form.js
@@ -1,5 +1,7 @@
-var error = $('<div class="message-form__tooltip message-form__tooltip_color_orange-red" />').text('Error');
-var thanks = $('<div class="message-form__tooltip message-form__tooltip_color_cyan" />').text('Thanks!');
+(() => {
+  const error = $('<div class="message-form__tooltip message-form__tooltip_color_orange-red"></div>').text('Error');
+  const thanks = $('<div class="message-form__tooltip message-form__tooltip_color_cyan" /></div>').text('Thanks!');
 
-$('.message-form .message-form__data-field_type_input:nth-of-type(1)').after(error);
-$('.message-form .message-form__data-field_type_input:nth-of-type(2)').after(thanks);
+  $('.message-form .message-form__data-field_type_input:nth-of-type(1)').append(error);
+  $('.message-form .message-form__data-field_type_input:nth-of-type(2)').append(thanks);
+})();
