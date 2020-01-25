517febe3700107eeec463363e58d2738bc3ed385
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-01-25 16:56:28 +0300

    install and set postcss-loader

--- webpack/style.extract.js
+++ webpack/style.extract.js
@@ -9,14 +9,36 @@ module.exports = function() {
                     use: ExtractTextPlugin.extract({
                         publicPath: '../',
                         fallback: 'style-loader',
-                        use: ['css-loader','sass-loader'],
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: { sourceMap: true },
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: { sourceMap: true, config: { path: './postcss.config.js' } },
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: { sourceMap: true },
+                            },
+                        ],
                     }),
                 },
                 {
                     test: /\.css$/,
                     use: ExtractTextPlugin.extract({
                         fallback: 'style-loader',
-                        use: 'css-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: { sourceMap: true },
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: { sourceMap: true, config: { path: './postcss.config.js' } },
+                            },
+                        ],
                     }),
                 }
             ]
