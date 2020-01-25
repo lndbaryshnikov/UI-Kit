49730ef257cd01018496283dfc96f11c7c24965c
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-01-25 19:51:39 +0300

    set js scss auto-import in ui-kit-demo-page.js

--- src/pages/ui-kit-demo-page/ui-kit-demo-page.js
+++ src/pages/ui-kit-demo-page/ui-kit-demo-page.js
@@ -1,46 +1,15 @@
-//ui-kit-demo-page components styles
 import '../../styles/common.scss'
 
-import '../../components/ui-kit-components/button/button'
-import '../../components/ui-kit-components/button/button.scss'
-import '../../components/ui-kit-components/arrow-buttons/arrow-buttons'
-import '../../components/ui-kit-components/arrow-buttons/arrow-buttons.scss'
-import '../../components/ui-kit-components/donut-chart/donut-chart.scss'
-import '../../components/ui-kit-components/slider/slider'
-import '../../components/ui-kit-components/slider/slider.scss'
-import '../../components/ui-kit-components/message-form/message-form'
-import '../../components/ui-kit-components/message-form/message-form.scss'
-import '../../components/ui-kit-components/toggle/toggle.scss'
-import '../../components/ui-kit-components/tick-box/tick-box.scss'
-import '../../components/ui-kit-components/search-bar/search-bar.scss'
-import '../../components/ui-kit-components/dropdown/dropdown.scss'
-import '../../components/ui-kit-components/user-profile/user-profile.scss'
-import '../../components/ui-kit-components/billboard/billboard.scss'
-import '../../components/ui-kit-components/message-box/message-box'
-import '../../components/ui-kit-components/message-box/message-box.scss'
-import '../../components/ui-kit-components/map/map'
-import '../../components/ui-kit-components/map/map.scss'
-import '../../components/ui-kit-components/datepicker/datepicker'
-import '../../components/ui-kit-components/datepicker/datepicker.scss'
-import '../../components/ui-kit-components/video/video.scss'
+function requireAll(r) {
+  return r.keys().map(r);
+}
 
-//ui-kit-demo-page page's styles
-import '../../components/demo-page-components/header/header.scss'
-import '../../components/demo-page-components/block-sign/block-sign.scss'
-import '../../components/demo-page-components/buttons/buttons.scss'
-import '../../components/demo-page-components/arrows/arrows.scss'
-import '../../components/demo-page-components/charts/charts.scss'
-import '../../components/demo-page-components/sliders/sliders.scss'
-import '../../components/demo-page-components/stages-box/stages-box.scss'
-import '../../components/demo-page-components/form-elements/form-elements.scss'
-import '../../components/demo-page-components/toggles/toggles.scss'
-import '../../components/demo-page-components/tick-boxes/tick-boxes.scss'
-import '../../components/demo-page-components/search-dropdown-profiles/search-dropdown-profiles.scss'
-import '../../components/demo-page-components/search-dropdown/search-dropdown.scss'
-import '../../components/demo-page-components/profile/profile.scss'
-import '../../components/demo-page-components/news-events/news-events.scss'
-import '../../components/demo-page-components/location/location.scss'
-import '../../components/demo-page-components/calendar-messaging/calendar-messaging.scss'
-import '../../components/demo-page-components/calendar/calendar.scss'
-import '../../components/demo-page-components/messaging/messaging.scss'
-import '../../components/demo-page-components/video-box/video-box.scss'
+//ui-kit-components styles
+requireAll(require.context('../../components/ui-kit-components', true, /\.js|scss$/));
+
+//ui-kit-demo-page components styles
+requireAll(require.context('../../components/demo-page-components', true, /\.js|scss$/));
+
+
+
+
