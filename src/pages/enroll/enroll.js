52342bb647057a6ff667ff9caac7b3000ea7f602
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-01-31 15:07:37 +0300

    delete enroll-form (expand message-form with enroll-form functionality)

--- src/pages/enroll/enroll.js
+++ src/pages/enroll/enroll.js
@@ -1,5 +1,5 @@
 import '../../components/site-components/menu/menu'
-import '../../components/site-components/enroll-form/enroll-form'
+import '../../components/ui-kit-components/message-form/message-form'
 import '../../components/ui-kit-components/datepicker/datepicker'
 
 import './enroll.scss'
