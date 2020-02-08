6b360e3e87870936c2e4eae694612a476ab9f571
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-02-08 13:12:08 +0300

    modify datepicker

--- src/components/ui-kit-components/datepicker/Datepicker.js
+++ src/components/ui-kit-components/datepicker/Datepicker.js
@@ -4,25 +4,47 @@ class Datepicker {
   constructor($element) {
     this._$datepickerContainer = $element;
 
+    this._defineElements();
     this.init();
   }
 
   init() {
-    this._$datepicker = this._$datepickerContainer.find('.js-datepicker__datepicker-wrapper');
-    this._$datepickerHeader = this._$datepickerContainer.find('.js-datepicker__header');
-
     this._$datepicker.datepicker({
-      classes: {
-        'ui-datepicker-current': 'datepicker__today-button',
-      },
       firstDay: 1,
       dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
       showButtonPanel: true,
       showOtherMonths: true,
       selectOtherMonths: true,
+      onSelect: this._displayCurrentDay.bind(this),
     });
 
-    this._$datepickerHeader.append(String(new Date().getDate()));
+    this._$todayButton.on('click', this._todayButtonClickHandler.bind(this));
+
+    this._displayCurrentDay();
+  }
+
+  _defineElements() {
+    const container = this._$datepickerContainer;
+
+    this._$datepicker = container.find('.js-datepicker__datepicker-wrapper');
+    this._$currentDay = container.find('.js-datepicker__current-day');
+    this._$todayButton = container.find('.js-datepicker__today-button');
+
+    console.log(container, this._$todayButton);
+  }
+
+  _todayButtonClickHandler() {
+    const todayDate = new Date();
+    console.log('im here');
+
+    this._$datepicker.datepicker('setDate', todayDate);
+    this._displayCurrentDay();
+  }
+
+  _displayCurrentDay() {
+    const currentDay = this._$datepicker.datepicker('getDate').getDate();
+
+    this._$currentDay.text(currentDay);
   }
 }
 
