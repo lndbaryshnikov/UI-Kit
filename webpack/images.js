7cc09dd2c75e9cd01a9b6c65771bf58219f0acb4
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-02-11 22:53:53 +0300

    set image optimizer

--- webpack/images.js
+++ webpack/images.js
@@ -5,16 +5,36 @@ module.exports = function () {
     module: {
       rules: [
         {
-          test: /\.(jpg|png|svg)$/,
+          test: /\.(jpe?g|png|svg)$/,
           include: path.resolve(__dirname, '../src'),
           exclude: [
             path.resolve(__dirname, '../src/fonts'),
             path.resolve(__dirname, '../src/favicons'),
           ],
-          loader: 'file-loader',
-          options: {
-            name: 'images/[name].[ext]',
-          },
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'images/[name].[ext]',
+              },
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65,
+                },
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4,
+                },
+              },
+            },
+          ],
         },
       ],
     },
