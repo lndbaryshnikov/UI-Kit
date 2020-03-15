4a0bf73a74842f8c75bd5206f6598da9eef57564
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-03-15 11:49:03 +0300

    fix image-loader-webpack config #15

--- webpack/images.js
+++ webpack/images.js
@@ -25,9 +25,6 @@ module.exports = function () {
                   progressive: true,
                   quality: 65,
                 },
-                optipng: {
-                  enabled: false,
-                },
                 pngquant: {
                   quality: [0.65, 0.90],
                   speed: 4,
